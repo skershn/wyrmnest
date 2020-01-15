eb8f2a7b0204d22cb5538767f1d50bc8169b9ec4
Shanna Kershner <shanna.kershner@gmail.com>
2020-01-15 05:41:33 +0000

    fixed migration error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_11_230045) do
+ActiveRecord::Schema.define(version: 2020_01_15_031020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,21 @@ ActiveRecord::Schema.define(version: 2020_01_11_230045) do
     t.datetime "updated_at", null: false
     t.index ["topic_id"], name: "index_comments_on_topic_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "dragon_types", force: :cascade do |t|
+    t.string "description"
+    t.integer "rarity"
+  end
+
+  create_table "dragons", force: :cascade do |t|
+    t.datetime "remember_created_at"
+    t.bigint "user_id"
+    t.bigint "dragon_type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dragon_type_id"], name: "index_dragons_on_dragon_type_id"
+    t.index ["user_id"], name: "index_dragons_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
