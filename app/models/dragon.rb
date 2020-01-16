4243c051bd00cd887be6d9e047ad1c26fac93638
Shanna Kershner <shanna.kershner@gmail.com>
2020-01-16 13:25:40 +0000

    updated models

--- app/models/dragon.rb
+++ app/models/dragon.rb
@@ -1,5 +1,4 @@
 class Dragon < ApplicationRecord
   belongs_to :user
   belongs_to :dragon_type
-  has_one :dragon_type
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,16 @@ class User < ApplicationRecord
   has_many :dragons
   has_one_attached :avatar
 
-#  def clicks
-#    self.clicks += 1
-#    self.save
-#    case self.clicks
-#    when 5
-#      dragon_type = DragonType.new(rarity:0).order(:random).first
-#      dragon_type = DragonType.create(rarity: 0, description: "A large green and blue egg. Somewhat like a large chicken egg.")
-#      d = Dragon.new(dragon_type: dragon_type, user: self)
-#    end
-#  end
+  def comment
+    self.comments += 1
+    self.save
+    case self.comments
+    when 5
+      dragon_type = DragonType.new(rarity:0).order(:random).first
+      dragon_type = DragonType.create(rarity: 0, description: "A large green and blue egg. Somewhat like a large chicken egg.")
+      d = Dragon.new(dragon_type: dragon_type, user: self)
+      d.save
+    end
+  end
 
 end
