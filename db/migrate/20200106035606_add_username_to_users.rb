e4780afd5feac16ad89dde0808d32bffd2e590e4
Shanna Kershner <shanna.kershner@gmail.com>
2020-01-19 22:25:06 +0000

    fixed db, updated dragon controller

--- db/migrate/20200106035606_add_username_to_users.rb
+++ db/migrate/20200106035606_add_username_to_users.rb
@@ -1,5 +1,4 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.2]
-  attr_accessible :username
   def change
     add_column :users, :username, :string
     add_index :users, :username, unique: true
